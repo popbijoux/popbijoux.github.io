5a61c4ceca4ce92a72f03e1fad7f14e60cec3120
popbijoux <manuelaleal2@gmail.com>
2017-02-07 00:23:53 -0500

    Add files via upload

---
+++ plssr/Plusser_style.js
@@ -0,0 +1,9 @@
+<script>
+ $( ".stEtienne" ).click(function() {
+      $(".StEtienneOnlyLove").typed({
+        strings: ["When you are young, and on your own, how does it feel to be alone?"],
+        typeSpeed: 100
+      });
+  });
+
+</script>
---
+++ plssr/plusser.js
@@ -0,0 +1,20 @@
+<script>
+ $( ".stEtienne" ).click(function() {
+      $(".StEtienneOnlyLove").typed({
+        strings: ["When you are young, and on your own, how does it feel to be alone?"],
+        typeSpeed: 100
+      });
+  });
+
+</script>
+
+
+<script>
+ $( ".stEtienne" ).click(function() {
+      $(".StEtienneOnlyLove").typed({
+        strings: ["U R Muslim terroriss from syrian counitres includen isreeal and franch."],
+        typeSpeed: 100
+      });
+  });
+
+</script>
