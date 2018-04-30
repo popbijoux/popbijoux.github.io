1de764dad148bb6e391727729756d467aee99a1e
Manuela Leal <manuelaleal@Manuelas-Air.wireless.newschool.edu>
2018-04-30 19:31:18 -0400

    list of commands

---
+++ Iphone/listOfCommandsPerVideo.js
@@ -0,0 +1,35 @@
+//She was warned
+//She was given an explanation
+//Nevertheless she persisted
+//Gen-Z Kids are not afraid
+//America is home to all
+//no ban
+//no wall
+//we all live in this small planet
+//We all cherish our children's future
+//We are all mortal
+//I came, I saw, I conquered
+//I went to a ball
+//I got a trophy
+//we are the people, we got a Story
+//just say no
+//this nation will rise up
+
+emmaGonzalesSpeechCropped.mp4
+sheWasWarned.mp4
+
+<div id="videoSheWasWarned" class="topVid">
+<div class="vidTitleText">SPEECHES: tear it down block the wall</div>
+    <video loop onclick="this.paused? this.play() : this.pause()">
+    <source src="videos/sheWasWarned.mp4" type="video/mp4">
+    Your browser does not support the video tag.
+    </video>
+</div>
+
+<div id="videoEmma" class="topVid">
+<div class="vidTitleText">SPEECHES: Emma Gonzales speech</div>
+    <video loop onclick="this.paused? this.play() : this.pause()">
+    <source src="videos/emmaGonzalesSpeechCropped.mp4" type="video/mp4">
+    Your browser does not support the video tag.
+    </video>
+</div>
