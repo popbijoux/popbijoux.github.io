4307ca6f326e23bd6e041730c9c9b5575351f82f
popbijoux <manuelaleal2@gmail.com>
2021-04-15 15:57:05 -0400

    Add files via upload

---
+++ lewis-map/data/markers.js
@@ -0,0 +1,92 @@
+var markers = [
+
+
+  //Add event location below
+//Sierra Madre, CA
+//Add event title below
+//John Lewis Votercade
+//add Coordinates for marker here
+/* "coordinates": [-118.052971, 34.16035]  */
+//add link here
+//https://www.mobilize.us/publiccitizen/event/382792/
+{
+  "name": "John Lewis Votercade, Sierra Madre, CA",
+  "url": "https://www.mobilize.us/publiccitizen/event/382792/",
+  /* "coordinates": [-118.052971, 34.16035]  */
+  "lat": 34.16035,
+  "lng": -118.05297
+
+},
+
+{
+  "name": "John Lewis Votercade, Kingston, NY",
+  "url": "https://www.mobilize.us/publiccitizen/event/382792/",
+  /* "coordinates": [-118.052971, 34.16035]  */
+  "lat": 41.9287812,
+  "lng": -74.0023702
+
+},
+
+{
+  "name": "John Lewis Votercade, Portland, ME",
+  "url": "https://www.mobilize.us/publiccitizen/event/382485",
+  /* "coordinates": [-118.052971, 34.16035]  */
+  "lat": 43.66249,
+  "lng": -70.254556
+
+},
+
+{
+  "name": "John Lewis Votercade in Ann Arbor, MI",
+  "url": "https://www.mobilize.us/publiccitizen/event/381547",
+  /* "coordinates": [-118.052971, 34.16035]  */
+  "lat": 42.2682,
+  "lng": -83.7312
+
+},
+{
+  "name": "John Lewis Votercade in Columbia, SC",
+  "url": "https://www.mobilize.us/publiccitizen/event/382727/",
+  /* "coordinates": [-118.052971, 34.16035]  */
+  "lat": 33.996145,
+  "lng": -81.034986
+
+},
+  {
+    "name": "John Lewis Votercade in Atlanta, GA",
+    "url": "https://www.mobilize.us/publiccitizen/event/382524/",
+    /* "coordinates": [-118.052971, 34.16035]  */
+    "lat": 33.76058,
+    "lng": -84.357739
+
+  },
+
+  {
+    "name": "John Lewis Votercade in Selma, AL",
+    "url": "https://www.mobilize.us/publiccitizen/event/382963/",
+    /* "coordinates": [-118.052971, 34.16035]  */
+    "lat": 32.407899,
+    "lng": -87.0207
+
+  },
+
+  {
+    "name": "John Lewis Votercade in Doral, FL",
+    "url": "https://www.mobilize.us/publiccitizen/event/383271",
+    /* "coordinates": [-118.052971, 34.16035]  */
+    "lat": 25.819499,
+    "lng": -80.355
+
+  },
+
+  {
+    "name": "John Lewis Votercade in San Rafael, CA",
+    "url": "https://www.mobilize.us/publiccitizen/event/382825",
+    /* "coordinates": [-118.052971, 34.16035]  */
+    "lat": 37.973229,
+    "lng": -122.53075
+
+  }
+
+
+];
